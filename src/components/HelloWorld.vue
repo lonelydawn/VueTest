053d4a25c15267124704b0b592833acabd195419
lonelydawn <lonelydawn@sina.com>
2018-05-21 16:37:39 +0800

    测试生命周期钩子函数的调用

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,113 +1,75 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div>
+    <h1 ref="h1" v-visible=false>{{ msg }}</h1>
+    <h2>Number: {{ number }}</h2>
+    <input v-model="number" type="text" v-focus><br><br>
+    <button @click="randomNumber">click</button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  data: () => ({
+    msg: 'This is a Vue app.',
+    profile: 'Hello universe!',
+    number: 0
+  }),
+  beforeCreate () {
+    console.log('before create')
+    this.$nextTick(function () {
+      console.log('before create next tick')
+    })
+  },
+  created () {
+    console.log('created')
+    this.$nextTick(function () {
+      console.log('created next tick')
+    })
+  },
+  beforeMount () {
+    console.log('before mount')
+    this.$nextTick(function () {
+      console.log('before mount next tick')
+    })
+  },
+  mounted () {
+    console.log('mounted')
+    this.$nextTick(function () {
+      console.log('mounted next tick')
+    })
+  },
+  beforeUpdate () {
+    console.log('before update')
+    this.$nextTick(function () {
+      console.log('before update next tick')
+    })
+  },
+  updated () {
+    console.log('updated')
+    this.$nextTick(function () {
+      console.log('updated next tick')
+    })
+  },
+  beforeDestroy () {
+    console.log('before destory')
+    this.$nextTick(function () {
+      console.log('before destory next tick')
+    })
+  },
+  destoryed () {
+    console.log('destoryed')
+    this.$nextTick(function () {
+      console.log('destoryed next tick')
+    })
+  },
+  methods: {
+    randomNumber () {
+      this.number = Math.floor(Math.random() * 100)
     }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<style scoped></style>
