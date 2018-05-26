5ba5526b25872372cb6dd07a355d5beb24f6131b
lonelydawn <lonelydawn@sina.com>
2018-05-26 19:22:02 +0800

    测试绑定的事件

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <h1 ref="h1" v-visible=false>{{ msg }}</h1>
+    <h1 ref="h1">{{ msg }}</h1>
     <h2>Number: {{ number }}</h2>
-    <input v-model="number" type="text" v-focus><br><br>
+    <input v-model="number" type="text"><br><br>
     <button @click="randomNumber">click</button>
   </div>
 </template>
@@ -12,57 +12,8 @@ export default {
   name: 'HelloWorld',
   data: () => ({
     msg: 'This is a Vue app.',
-    profile: 'Hello universe!',
     number: 0
   }),
-  beforeCreate () {
-    console.log('before create')
-    this.$nextTick(function () {
-      console.log('before create next tick')
-    })
-  },
-  created () {
-    console.log('created')
-    this.$nextTick(function () {
-      console.log('created next tick')
-    })
-  },
-  beforeMount () {
-    console.log('before mount')
-    this.$nextTick(function () {
-      console.log('before mount next tick')
-    })
-  },
-  mounted () {
-    console.log('mounted')
-    this.$nextTick(function () {
-      console.log('mounted next tick')
-    })
-  },
-  beforeUpdate () {
-    console.log('before update')
-    this.$nextTick(function () {
-      console.log('before update next tick')
-    })
-  },
-  updated () {
-    console.log('updated')
-    this.$nextTick(function () {
-      console.log('updated next tick')
-    })
-  },
-  beforeDestroy () {
-    console.log('before destory')
-    this.$nextTick(function () {
-      console.log('before destory next tick')
-    })
-  },
-  destoryed () {
-    console.log('destoryed')
-    this.$nextTick(function () {
-      console.log('destoryed next tick')
-    })
-  },
   methods: {
     randomNumber () {
       this.number = Math.floor(Math.random() * 100)
@@ -70,6 +21,3 @@ export default {
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
